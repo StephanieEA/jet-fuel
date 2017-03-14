88d3937ffa22dbb1265669e58a69f7b83da24a05
Stephanie Andrews <seandrews11@gmail.com>
2017-03-14 14:29:54 -0600

    Add endpoint for folders and urls

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var express = require('express')
+var md5 = require('md5')
 var app = express()
 
 app.use(express.static('public'))
+
+app.locals.folders = [{
+  id: md5('animals'),
+  name: 'animals'
+}]
+
+app.locals.urls = [{
+  id: md5('animals.com'),
+  source: 'animals.com',
+  short: `localhost:3000/${md5('animals.com')}`
+}]
+
+app.get('/api/v1/folders', (req, res) => {
+  res.json(app.locals.folders)
+})
+
+app.get('/api/v1/urls', (req, res) => {
+  res.json(app.locals.urls)
+})
 // respond with "hello world" when a GET request is made to the homepage
 // app.get('/', function (req, res) {
 //   res.send('hello world')

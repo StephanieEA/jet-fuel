dd4046691d0b1390d69a713e410b79e2fe92c94a
Eric Sayler <easayler@gmail.com>
2017-03-15 17:04:26 -0600

    set up knex, add migrations and seeds

---
+++ db/migrations/20170315152149_add-urls-folders-tables.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('folders', function(table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.timestamps(true, true)
+    }),
+    knex.schema.createTable('urls', function(table) {
+      table.increments('id').primary()
+      table.string('long_url')
+      table.integer('folder_id').unsigned()
+      table.foreign('folder_id').references('folders.id')
+      table.integer('visits')
+      table.timestamps(true, true)
+    }),
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+       knex.schema.dropTable('urls'),
+       knex.schema.dropTable('folders')
+   ])
+};
---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: 'postgres://localhost/jet-fuel'
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/jet-fuel_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};

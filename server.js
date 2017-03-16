f0edfee8fd65ca9584a813889bc3b4bdbc02e5e8
Eric Sayler <easayler@gmail.com>
2017-03-15 19:02:21 -0600

    add db to api endpoints, fix frontend

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 const express = require('express')
 const md5 = require('md5')
 const bodyParser =require('body-parser')
@@ -13,88 +17,114 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.locals.folders = [{
-  id: md5('animals'),
-  name: 'animals'
-}, {
-  id: md5('reptiles'),
-  name: 'reptiles'
-}]
-
-app.locals.urls = [{
-  id: md5('animals.com'),
-  folderId: md5('animals'),
-  source: 'http://animals.com',
-  short: `http://localhost:3000/${md5('animals.com')}`,
-  visits: 0,
-  createdAt: Date.now()
-}, {
-  id: md5('reptiles.com'),
-  folderId: md5('reptiles'),
-  source: 'http://reptiles.com',
-  short: `http://localhost:3000/${md5('reptiles.com')}`,
-  visits: 0,
-  createdAt: Date.now()
-}]
-
 // get all folders
 app.get('/api/v1/folders', (req, res) => {
-  res.json(app.locals.folders)
+  database('folders').select()
+    .then(function(folders) {
+      res.status(200).json(folders);
+    })
+    .catch(function(error) {
+      console.error(chalk.red('somethings wrong with db'))
+      res.sendStatus(500)
+    });
 })
 
 // get all urls
 app.get('/api/v1/urls', (req, res) => {
-  res.json(app.locals.urls)
+  database('urls').select()
+    .then(function(urls) {
+      res.status(200).json(urls);
+    })
+    .catch(function(error) {
+      console.error(chalk.red('somethings wrong with db'))
+      res.sendStatus(500)
+    });
 })
 
 // get a specific folder
 app.get('/api/v1/folders/:id', (req, res) => {
   const { id } = req.params
-  const folder = app.locals.folders.find(folder => folder.id === id)
-
-  if (!folder) return response.sendStatus(404)
-  res.json(folder)
+  database('folders').where('id', id)
+    .then(function(folders) {
+      res.status(200).json(folders);
+    })
+    .catch(function(error) {
+      console.error(chalk.red('somethings wrong with db'))
+      res.sendStatus(500)
+    });
 })
 
 // get all of the urls for a specific folder
 app.get('/api/v1/folders/:id/urls', (req, res) => {
   const { id } = req.params
-  const urls = app.locals.urls.filter(url => url.folderId === id)
-
-  if (!urls) return response.sendStatus(404)
-  res.json(urls)
+  database('urls').where('folder_id', id)
+    .then(function(folders) {
+      res.status(200).json(folders);
+    })
+    .catch(function(error) {
+      console.error(chalk.red('somethings wrong with db'))
+      res.sendStatus(500)
+    });
 })
 
 // add a folder
 app.post('/api/v1/folders', (req, res) => {
   const { name } = req.body
-  const id = md5(name)
-
-  app.locals.folders.push({ id, name })
-  res.json(app.locals.folders)
+  const folder = { name }
+  database('folders').insert(folder)
+    .then(function() {
+      database('folders').select()
+        .then(function(folders) {
+          res.status(200).json(folders);
+        })
+        .catch(function(error) {
+          console.error(chalk.red('error adding a folder to db: ', JSON.stringify(error)))
+        });
+     })
 })
 
 // add a url to a specific folder
 app.post('/api/v1/folders/:id/urls', (req, res) => {
-  const { source } = req.body
-  const folderId = req.params.id
-  const id = md5(source)
-  const short = `http://localhost:3000/${id}`
-  const createdAt = Date.now()
-  const visits = 0
+  const { long_url } = req.body
+  const folder_id = req.params.id
+  const url = { folder_id, long_url, visits: 0 }
 
-  app.locals.urls.push({ id, folderId, createdAt, visits, source, short})
-  const urls = app.locals.urls.filter(url => url.folderId === folderId)
-  res.json(urls)
+  database('urls').insert(url)
+    .then(function() {
+      database('urls').where('folder_id', folder_id)
+        .then(function(urls) {
+          res.status(200).json(urls);
+        })
+        .catch(function(error) {
+          console.error(chalk.red('error adding a folder to db: ', JSON.stringify(error)))
+        })
+     })
+
 })
 
-// redirect to the source of a shortened url
+// redirect to the long_url of a shortened url
 app.get('/:id', (req, res) => {
   const { id } = req.params
-  console.log(id)
-  const url = app.locals.urls.find(url => url.id === id)
-  url.visits += 1
-  res.redirect(`http://${url.source}`)
+  if (id === 'favicon.ico') {
+    return
+  }
+
+  database('urls').where('id', id).increment('visits', 1)
+  .then(function() {
+    database('urls').where('id', id)
+      .then(function(urls) {
+        if (urls[0].long_url === null) {
+          res.sendStatus(404)
+        }
+        res.redirect(`http://${urls[0].long_url}`)
+      })
+      .catch(function(error) {
+        console.error(chalk.red('error redirecting to shortend url', JSON.stringify(error)))
+      })
+  })
+
+
+  // TODO: url.visits += 1
 })
 
 app.listen(app.get('port'), () => {
